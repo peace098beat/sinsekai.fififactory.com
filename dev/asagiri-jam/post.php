69853bb05c1c6eb89ae65b18e760ac8142250498
Lenovo <peace098beat.t@gmail.com>
2014-10-09 00:34:03 +0900

    データベースの初期化関数の実装。ファイル名に"-"が使えないことにはまった。

--- dev/asagiri-jam/post.php
+++ dev/asagiri-jam/post.php
@@ -4,39 +4,8 @@
 /**********************************************************/
 // header("Content-Type: text/html; charset=UTF-8");
 // 文字コードの設定
-// try {
-// 	// if( $data = $_FILES["upfile"] ){
-// 	// move_uploaded_file( $data["tmp_name"], "./updata/".date(U)."-".$data["name"] );
-// 	// move_uploaded_file( $data["tmp_name"], "./updata/".$data["name"] );
-// 	// echo("<img src='"."./img/".date(U).$data["name"]."' alt=''>");
-// 	// echo("<video src='"."./img/".date(U).$data["name"]."' alt=''>");
 
-// 	// echo "昔の方法ではせいこうです。";
-// 	// }else{
-// 	// echo "nono";
-// 	// echo "昔の方法では失敗です";
-// 	// }
-// } catch (Exception $e) {
-// 	// echo $e;
-	
-// }
-
-
-// var_dump($_FILES);
 // print_r($_FILES);
-// print_r($_POST);
-
-// http://www.tagindex.com/html5/form/input_file.html
-// if (is_uploaded_file($_FILES["upfile"]["tmp_name"])) {
-//   if (move_uploaded_file($_FILES["upfile"]["tmp_name"], "./updata/".date(U)."-".$_FILES["upfile"]["name"])) {
-//     chmod("./updata/".date(U)."-".$_FILES["upfile"]["name"], 0644);
-//     // echo $_FILES["upfile"]["name"] . "をアップロードしました。";
-//   } else {
-//     echo "<p>ファイルをアップロードできませんでした。</p>";
-//   }
-// } else {
-//   echo "<p>ファイルが選択されていません。";
-// }
 
 // 複数ファイルに対応
 $file_n = count($_FILES["upfile"]["tmp_name"]);
@@ -49,17 +18,87 @@ for ($i=0; $i < $file_n; $i++) {
 	  if (move_uploaded_file($_FILES["upfile"]["tmp_name"][$i], $fname)) {
 	  	// ファイルのパーミッションを変更
 	    chmod($fname, 0644);
-	    echo $_FILES["upfile"]["name"] . "をアップロードしました。";
+	    // echo $_FILES["upfile"]["name"] . "をアップロードしました。";
 	  } else {
-	    echo "<p>ファイルをアップロードできませんでした。</p>";
+	    // echo "<p>ファイルをアップロードできませんでした。</p>";
 	  }
 	} else {
-	  echo "<p>ファイルが選択されていません。";
+	  // echo "<p>ファイルが選択されていません。";
 	}
 }
 
-$link1 = "http://shinsekai.fififactory.com/dev/asagiri-jam/index.php";
-$link2 = "http://shinsekai.fififactory.com/dev/asagiri-jam/Grid-A-Licious.php";
+/**********************************************************
+/ データベースへの保存
+/**********************************************************/
+//テーブル名 (ハイフン"-"は使用不可)
+$TBNAME = "asagiridev";
+
+// データベースの初期化
+tbclear($TBNAME);
+
+// データベースへ接続
+$conn = init();
+
+// データベースの初期設定 
+function init() {
+		// データベースに接続
+		$dsn = "mysql:dbname=sddb0040169390;host=sddb0040169390.cgidb";
+		$user = 'sddbNDg4NjQ3';
+		$password = 'wQ5vs7MD';
+
+		try{
+			$conn = new PDO($dsn, $user, $password);
+		}catch(PDOException $e){
+			print('Error:'.$e->getMessage());
+		die();
+		}
+		// 文字化け対策
+		$sql = "SET NAMES utf8";
+		$stmt = $conn->prepare($sql);
+		$stmt->execute();
+		return $conn;
+}
+
+// DBを初期化
+function tbclear($TBNAME){
+
+	$conn = init();
+	// 既存のテーブルを削除
+	try{
+	$sql="DROP TABLE ".$TBNAME;
+	$stmt = $conn->prepare($sql);
+	$stmt->execute();
+	}catch(PDOException $e){
+		print('Error:'.$e->getMessage());
+		die();
+	}
+
+
+	$sql="CREATE TABLE IF NOT EXISTS ".$TBNAME."(
+		id INTEGER PRIMARY KEY AUTO_INCREMENT,
+		user VARCHAR(50) NOT NULL,
+		publishd VARCHAR(50) NOT NULL,
+		story VARCHAR(50) ,
+		plan VARCHAR(50) ,
+		memo TEXT,
+		ordmaild VARCHAR(50) ,
+		phogetd VARCHAR(50) ,
+		scand VARCHAR(50) ,
+		mkvideod VARCHAR(50) ,
+		mkdvdd VARCHAR(50) ,
+		postd VARCHAR(50) ,
+		deliverd VARCHAR(50) ,
+		dt DATETIME
+	)";
+	$stmt = $conn->prepare($sql);
+	$stmt->execute();
+	$conn = null;
+}
+
+
+
+// $link1 = "http://shinsekai.fififactory.com/dev/asagiri-jam/index.php";
+// $link2 = "http://shinsekai.fififactory.com/dev/asagiri-jam/Grid-A-Licious.php";
 
 // header("HTTP/1.1 301 Moved Permanently");
 // header("Location: $link2");
